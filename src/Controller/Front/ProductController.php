2c3ab3e1559b5a5a60a5d1087f1119ddc32b4001
DamienCH33 <damienchauveau@hotmail.fr>
2025-12-23 15:01:59 +0100

    creation front controller + termplates

--- src/Controller/Front/ProductController.php
+++ src/Controller/Front/ProductController.php
@@ -5,9 +5,11 @@ namespace App\Controller\Front;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 final class ProductController extends AbstractController
 {
@@ -34,9 +36,10 @@ final class ProductController extends AbstractController
     #[IsGranted('ROLE_USER')]
     public function showProductList(): Response
     {
-        $products = $this->em->getRepository(Product::class)->findAll();   
+        $products = $this->em->getRepository(Product::class)->findAll();
+
         return $this->render('front/productList.html.twig', [
             'products' => $products,
-        ]); 
+        ]);
     }    
 }
