c2d2495bd0c9f7ae7019ce7c51d6a65c1691a130
DamienCH33 <damienchauveau@hotmail.fr>
2025-12-30 09:45:42 +0100

    maj css

--- src/Controller/Api/ProductApiController.php
+++ src/Controller/Api/ProductApiController.php
@@ -5,10 +5,8 @@ namespace App\Controller\Api;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api', name: 'api_')]
 final class ProductApiController extends AbstractController
@@ -16,16 +14,10 @@ final class ProductApiController extends AbstractController
     public function __construct(private EntityManagerInterface $em) {}
 
     #[Route('/products', name: 'products_list', methods: ['GET'])]
-    public function products(SerializerInterface $serializer): Response
+    public function products(): Response
     {
         $products = $this->em->getRepository(Product::class)->findAll();
 
-        $jsonProducts = $serializer->serialize(
-            $products,
-            'json',
-            ['groups' => 'product:read']
-        );
-
-        return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
+        return  $this->json($products, Response::HTTP_OK, [], ['groups' => 'product:read']);
     }
 }
